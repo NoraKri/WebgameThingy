55d605f47045a7e68fcfd2c5e5f06708188a29b8
NoraKri <106915799+NoraKri@users.noreply.github.com>
2022-11-01 21:39:50 +0100

    Added cursors

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 var coins = 0;
+var cursors = 0;
 
 function getCoin(number) {
   coins += number;
   document.getElementById("coins").innerHTML = coins;
 }
+
+function buyCursor() {
+  var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
+  if (coins >= cursorCost) {
+    cursors++;
+    coins -= cursorCost;
+    document.getElementById("cursors").innerHTML = cursors;
+    document.getElementById("coins").innerHTML = coins;
+  }
+  var nextCost = Math.floor(10 * Math.pow(1.1, cursors));
+  document.getElementById("cursorCost").innerHTML = cursorCost;
+}
+
+// Game loop
+window.setInterval(function () {
+  //Everything in here happens once a second
+  getCoin(cursors);
+  console.log(coins);
+}, 1000);
