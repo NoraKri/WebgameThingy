041e7a0ce0d5edd1ff23795d49b343f92f28efe8
NoraKri <106915799+NoraKri@users.noreply.github.com>
2022-11-04 22:59:23 +0100

    Trying out some enemy stuff

--- main.js
+++ main.js
@@ -1,47 +1,44 @@
 var Game = {};
 
 var coins = 0;
-var cursors = 0;
+var arrows = 0;
+var enemyHealth = 20;
 const gameWindow = document.getElementById("backgroundLeftCanvas");
 const c = gameWindow.getContext("2d");
 
 gameWindow.width = 720;
 gameWindow.height = 920;
 
-c.fillStyle = "green";
-c.fillRect(0, 0, gameWindow.width, gameWindow.height);
-
-// Enemy spawning
-// Find the middle of the map (where the tower is)
-// Tower is located in the towerAnchor div.
-c.fillStyle = "red";
-c.fillRect(100, 200, 64, 64);
-
-function animateEnemy() {
-  requestAnimationFrame(animateEnemy);
+// Ha en enemy som et tekstelement paa skjermen, hver gang brukeren trykker paa attack, mister enemy liv, naar enemy doer, faa 2 coins.
+function attackEnemy(number) {
+  enemyHealth -= number;
+  if (enemyHealth <= 0) {
+    getCoin(2);
+    enemyHealth = 20;
+  }
+  document.getElementById("enemy").innerHTML = enemyHealth;
 }
 
-animateEnemy();
-
+// Get 2 coins when killing enemy
 function getCoin(number) {
   coins += number;
   document.getElementById("coins").innerHTML = coins + " coins";
 }
 
-function buyCursor() {
-  var cursorCost = Math.floor(10 * Math.pow(1.1, cursors));
-  if (coins >= cursorCost) {
-    cursors++;
-    coins -= cursorCost;
-    document.getElementById("cursors").innerHTML = cursors;
+function buyArrow() {
+  var arrowCost = Math.floor(10 * Math.pow(1.1, arrows));
+  if (coins >= arrowCost) {
+    arrows++;
+    coins -= arrowCost;
+    document.getElementById("arrow").innerHTML = arrows;
     document.getElementById("coins").innerHTML = coins;
   }
-  var nextCost = Math.floor(10 * Math.pow(1.1, cursors));
-  document.getElementById("cursorCost").innerHTML = cursorCost;
+  var nextCost = Math.floor(10 * Math.pow(1.1, arrows));
+  document.getElementById("arrowCost").innerHTML = arrowCost;
 }
 
 // Game loop
 window.setInterval(function () {
   //Everything in here happens once a second
-  getCoin(cursors);
+  attackEnemy(arrows);
 }, 1000);
