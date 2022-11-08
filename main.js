38387ad09afc18b1458520897172fb180fab24e1
NoraKri <106915799+NoraKri@users.noreply.github.com>
2022-11-08 11:41:55 +0100

    Enemies get added to an array, spawn every 5 sec

--- main.js
+++ main.js
@@ -3,20 +3,37 @@ var Game = {};
 var coins = 0;
 var arrows = 0;
 var enemyHealth = 20;
+const enemies = [];
+var enemyCounter = 0;
+
 const gameWindow = document.getElementById("backgroundLeftCanvas");
 const c = gameWindow.getContext("2d");
 
 gameWindow.width = 720;
 gameWindow.height = 920;
 
-// Ha en enemy som et tekstelement paa skjermen, hver gang brukeren trykker paa attack, mister enemy liv, naar enemy doer, faa 2 coins.
+// Spawn an enemy every x seconds, with x amount of health.
+window.setInterval(function () {
+  spawnEnemy(enemyHealth);
+}, 5000);
+
+// Create an enemy element inside the enemies div, set its ID to the amount of enemies spawned
+function spawnEnemy(health) {
+  enemyCounter++;
+  const enemy = { health: health, name: "Goblin no " + enemyCounter };
+  enemies.push(enemy);
+  updateEnemyList();
+}
+
+// Attack enemy with x amount of damage, if enemy has 0 or less health, delete the entity and give x amount of coins to the player
 function attackEnemy(number) {
-  enemyHealth -= number;
-  if (enemyHealth <= 0) {
+  const target = enemies[0];
+  target.health -= number;
+  if (target.health <= 0) {
     getCoin(2);
-    enemyHealth = 20;
+    enemies.shift();
   }
-  document.getElementById("enemy").innerHTML = enemyHealth;
+  updateEnemyList();
 }
 
 // Get 2 coins when killing enemy
@@ -35,6 +52,18 @@ function buyArrow() {
   }
   var nextCost = Math.floor(10 * Math.pow(1.1, arrows));
   document.getElementById("arrowCost").innerHTML = arrowCost;
+}
+
+function updateEnemyList() {
+  var html = enemies
+    .map(function (element) {
+      return (
+        "<li>" + element.name + ", " + element.health + " health" + "</li>"
+      );
+    })
+    .join("");
+
+  document.getElementById("enemies").innerHTML = html;
 }
 
 // Game loop
